36b000e2c63499cb5d3f4ecaf9cace7a465bd353
Stas <stas.shc@gmail.com>
2023-06-13 20:15:28 +0300

    add db

--- server/src/main/java/org/example/Main.java
+++ server/src/main/java/org/example/Main.java
@@ -9,15 +9,25 @@ import connection.ReadingObject;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.channels.SelectionKey;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.PriorityQueue;
+import java.util.Properties;
 import java.util.Set;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, SQLException {
+
+        Properties info = new Properties();
+        info.load(new FileInputStream("server/src/main/java/db/db.cfg"));
+        String jdbcURL = "jdbc:postgresql://localhost:4782/studs";
+        java.sql.Connection connectionDataBase = DriverManager.getConnection(jdbcURL, info);
 
         CollectionDirector<PriorityQueue<Vehicle>> collectionDirector =
                 new CollectionDirector<>(new PriorityQueue<>());
